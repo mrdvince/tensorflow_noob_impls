a79f8c622b93e11745eb3edf8d8d1394c24014bd
mrdvince <vincivenv@gmail.com>
2021-04-07 03:18:57 +0300

    efficientnets are really efficient

--- transfer_learning.py
+++ transfer_learning.py
@@ -2,7 +2,6 @@
 import os
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import tensorflow_hub as hub
@@ -13,7 +12,7 @@ from PIL import Image
 
 # %%
 # Use a tf hub efficient net for classification
-CLASSIFIER_URL = "https://tfhub.dev/tensorflow/efficientnet/b0/classification/1"
+CLASSIFIER_URL = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/2"  # "https://tfhub.dev/tensorflow/efficientnet/b0/classification/1"
 IMAGE_RES = 224
 model = tf.keras.Sequential(
     [hub.KerasLayer(CLASSIFIER_URL, input_shape=(IMAGE_RES, IMAGE_RES, 3))]
@@ -27,7 +26,7 @@ grace_hopper = tf.keras.utils.get_file(
 grace_hopper = Image.open(grace_hopper).resize((IMAGE_RES, IMAGE_RES))
 grace_hopper = np.array(grace_hopper) / 255  # all pixels between 0 and 1
 # %%
-# Efficient net miss classifiers this, try a mobile net instead
+# Efficient net mis-classifiers this, try a mobile net instead
 predicted_class = np.argmax(model.predict(grace_hopper[np.newaxis, ...])[0], axis=-1)
 labels_path = tf.keras.utils.get_file(
     "ImageNetLabels.txt",
@@ -48,8 +47,9 @@ _ = plt.title("Prediction: " + predicted_class_name.title())
     as_supervised=True,
     split=["train[:80%]", "train[80%:]"],
 )
+# %%
 num_examples = info.splits["train"].num_examples
-num_classes = info.splits["label"].num_classes
+num_classes = info.features["label"].num_classes
 
 # %%
 # images not same size, func to make them same size
@@ -76,11 +76,60 @@ result_batch = model.predict(images)
 
 predicted_class_names = imagenet_labels[np.argmax(result_batch, axis=-1)]
 
-plt.figure(figsize=(12, 8))
-for n in range(30):
-    plt.subplot(6, 5, n + 1)
-    plt.subplots_adjust(hspace=0.3)
-    plt.imshow(images[n])
-    plt.title(predicted_class_names[n])
-    plt.axis("off")
-    _ = plt.suptitle("Imagenet predictions")
+# plt.figure(figsize=(12, 8))
+# for n in range(30):
+#     plt.subplot(6, 5, n + 1)
+#     plt.subplots_adjust(hspace=0.3)
+#     plt.imshow(images[n])
+#     plt.title(predicted_class_names[n])
+#     plt.axis("off")
+#     _ = plt.suptitle("Imagenet predictions")
+
+# %%
+# transfer learning (efficient net lite)
+URL = "https://tfhub.dev/tensorflow/efficientnet/lite0/feature-vector/2"
+feature_extractor = hub.KerasLayer(URL, input_shape=(IMAGE_RES, IMAGE_RES, 3))
+
+# freeze
+feature_extractor.trainable = False
+
+# attach a classification head
+model = tf.keras.Sequential([feature_extractor, layers.Dense(2)])
+model.summary()
+
+# %%
+# train the model
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+# %%
+EPOCHS = 5
+model = model.fit(train_batches, epochs=EPOCHS, validation_data=validation_batches)
+
+# %%
+# plot stuff
+acc = model.history["accuracy"]
+val_acc = model.history["val_accuracy"]
+loss = model.history["loss"]
+val_loss = model.history["val_loss"]
+
+epochs_range = range(EPOCHS)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label="Training Accuracy")
+plt.plot(epochs_range, val_acc, label="Validation Accuracy")
+plt.legend(loc="lower right")
+plt.title("Training and Validation Accuracy")
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label="Training Loss")
+plt.plot(epochs_range, val_loss, label="Validation Loss")
+plt.legend(loc="upper right")
+plt.title("Training and Validation Loss")
+plt.show()
+
+# %%
